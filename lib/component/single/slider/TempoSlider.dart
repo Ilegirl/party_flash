bb5d8415e3183db81311400dda3c780d283fe4d1
mugoo <anisum47@gmail.com>
2024-05-12 21:37:47 +0900

    [feat] Slider UI 변경

--- lib/component/single/slider/TempoSlider.dart
+++ lib/component/single/slider/TempoSlider.dart
@@ -33,61 +33,166 @@ class _TempoSliderState extends State<TempoSlider> {
           Text(
             'Speed',
             style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-              color: Colors.black87,
+              fontSize: 30 * ScreenSize.scaleWidth(context),
+              color: Colors.white,
             ),
           ),
-          SizedBox(height: 20),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.58,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: _currentSliderValue == 0 ? Text('x0.5') : SizedBox(),
-                  ),
-                ),
-                Expanded(
-                  child: Center(
-                    child: _currentSliderValue == 50 ? Text('x1') : SizedBox(),
-                  ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: _currentSliderValue == 100 ? Text('x2') : SizedBox(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SliderTheme(
-            data: SliderThemeData(
-              trackHeight: 10,
-              inactiveTickMarkColor: Color(0xFFD9D9D9),
-              activeTrackColor: Color(0xFF797979),
-              inactiveTrackColor: Color(0xFF797979),
-              thumbColor: Color(0xFFD9D9D9)
-            ),
-            child: Slider(
-              value: _currentSliderValue,
-              max: 100,
-              divisions: 2,
-              onChanged: (double value) {
-                setState(() {
-                  _currentSliderValue = value;
-                });
-                if (widget.onChanged != null) {
-                  widget.onChanged!(value);
-                }
-              },
-            ),
+          SizedBox(height: 20 * ScreenSize.scaleWidth(context)),
+          CustomSlider(
+            value: _currentSliderValue,
+            onChanged: (value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(value);
+              }
+            },
           ),
         ],
       ),
     );
+  }
+}
+
+class CustomSlider extends StatefulWidget {
+  final double value;
+  final ValueChanged<double>? onChanged;
+
+  const CustomSlider({Key? key, required this.value, this.onChanged})
+      : super(key: key);
+
+  @override
+  _CustomSliderState createState() => _CustomSliderState();
+}
+
+class _CustomSliderState extends State<CustomSlider> {
+  late double _currentSliderValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentSliderValue = widget.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderTheme.of(context).copyWith(
+        trackHeight: 20 * ScreenSize.scaleWidth(context), // 슬라이더 바의 두께 조절
+        inactiveTickMarkColor: Color(0xFFD9D9D9),
+        activeTickMarkColor: Color(0xFFD9D9D9),
+        activeTrackColor: Color(0xFF797979),
+        inactiveTrackColor: Color(0xFF797979),
+        thumbColor: Color(0xFFD9D9D9),
+        thumbShape: DonutSliderThumbShape(
+          outerRadius: 17 * ScreenSize.scaleWidth(context), // thumb 바깥 반지름
+          innerRadius: 12 * ScreenSize.scaleWidth(context), // thumb 안쪽 반지름
+        ),
+      ),
+      child: Stack(
+        children: [
+          Slider(
+            value: _currentSliderValue,
+            min: 0,
+            max: 100,
+            divisions: 2,
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(value);
+              }
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 60 * ScreenSize.scaleWidth(context)), // 위쪽 패딩 추가
+                  child: Text(
+                    '   x0.5',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20 * ScreenSize.scaleWidth(context), // 글자 크기
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 60 * ScreenSize.scaleWidth(context)), // 위쪽 패딩 추가
+                  child: Text(
+                    'x1',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20 * ScreenSize.scaleWidth(context), // 글자 크기
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 60 * ScreenSize.scaleWidth(context)), // 위쪽 패딩 추가
+                  child: Text(
+                    '          x2',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20 * ScreenSize.scaleWidth(context), // 글자 크기
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DonutSliderThumbShape extends SliderComponentShape {
+  final double outerRadius;
+  final double innerRadius;
+
+  const DonutSliderThumbShape({
+    required this.outerRadius,
+    required this.innerRadius,
+  });
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(outerRadius);
+  }
+
+  @override
+  void paint(
+      PaintingContext context,
+      Offset center, {
+        required Animation<double> activationAnimation,
+        required Animation<double> enableAnimation,
+        required bool isDiscrete,
+        required TextPainter labelPainter,
+        required RenderBox parentBox,
+        required SliderThemeData sliderTheme,
+        required TextDirection textDirection,
+        required double value,
+        required double textScaleFactor,
+        required Size sizeWithOverflow,
+      }) {
+    final Canvas canvas = context.canvas;
+
+    final Paint outerCirclePaint = Paint()..color = Color(0xFFD9D9D9);
+    final Paint innerCirclePaint = Paint()..color = Color(0xFF7882DE);
+
+    final double innerRadius = outerRadius / 2;
+
+    canvas.drawCircle(center, outerRadius, outerCirclePaint);
+    canvas.drawCircle(center, innerRadius, innerCirclePaint);
   }
 }
