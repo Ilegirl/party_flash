75965d87c01c859b1a630301767382adba5a9422
hyeseon_han <hyeseon_han@tmax.co.kr>
2024-07-24 10:09:45 +0900

    [fix] 컴파일 에러 수정

--- lib/component/single/button/FlashButton.dart
+++ lib/component/single/button/FlashButton.dart
@@ -75,19 +75,3 @@ class _FlashButtonState extends State<FlashButton> {
     );
   }
 }
-
-import 'package:flutter/material.dart';
-
-class Crossmargin extends StatelessWidget {
-
-  const Crossmargin({Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400.0,
-      height: 60.0
-    );
-  }
-}
